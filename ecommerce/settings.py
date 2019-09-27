bf5705eaeac0e4ef9cb6253ca5c90c8c03c8f59c
Ubuntu <ubuntu@ip-172-31-24-127.ec2.internal>
2019-09-27 18:26:41 +0000

    Add 'ecommerce-rasquin.herokuapp.com/' to allowed host list

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('AWSC9_HOSTNAME')]
+ALLOWED_HOSTS = [os.environ.get('AWSC9_HOSTNAME'), 'ecommerce-rasquin.herokuapp.com/']
 
 
 # Application definition
