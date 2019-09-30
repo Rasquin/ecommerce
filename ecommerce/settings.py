3558fb341b07ba59a74683edd885ed73248d9e8e
Ubuntu <ubuntu@ip-172-31-24-127.ec2.internal>
2019-09-30 13:13:35 +0000

    commented impor env in settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 # To deploy to heroku you need to uncomment inmport view. You comment it when testeing with travis
-import env
+#import env
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
